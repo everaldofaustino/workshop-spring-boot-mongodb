9bd2874a46cd1d44932b263fd437450f70f85adb
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-06-19 12:09:55 -0300

    Obtendo um post por id

---
+++ src/main/java/com/everaldo/workshopmongo/resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.everaldo.workshopmongo.resources;
+
+import com.everaldo.workshopmongo.domain.Post;
+import com.everaldo.workshopmongo.domain.User;
+import com.everaldo.workshopmongo.dto.UserDTO;
+import com.everaldo.workshopmongo.services.PostService;
+import com.everaldo.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method=RequestMethod.GET)
+    public ResponseEntity< Post> findById(@PathVariable String id){
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/everaldo/workshopmongo/services/PostService.java
@@ -0,0 +1,31 @@
+package com.everaldo.workshopmongo.services;
+
+import com.everaldo.workshopmongo.domain.Post;
+import com.everaldo.workshopmongo.domain.User;
+import com.everaldo.workshopmongo.dto.UserDTO;
+import com.everaldo.workshopmongo.repository.PostRepository;
+import com.everaldo.workshopmongo.repository.UserRepository;
+import com.everaldo.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+        @Autowired
+        private PostRepository repo;
+
+        public Post findById(String id){
+            Optional<Post> user = repo.findById(id);
+            if(user.isEmpty()){
+                throw new ObjectNotFoundException("Objeto não encontrado");
+            }
+
+            return user.get();
+
+        }
+
+}
