1def46a2fd160dcf8e75dc4bfb7a493c4e3e6513
Everaldo Faustino <everaldo_faustino@yahoo.com.br>
2026-06-19 11:18:58 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/everaldo/workshopmongo/config/instantiation.java
+++ src/main/java/com/everaldo/workshopmongo/config/instantiation.java
@@ -2,6 +2,7 @@ package com.everaldo.workshopmongo.config;
 
 import com.everaldo.workshopmongo.domain.Post;
 import com.everaldo.workshopmongo.domain.User;
+import com.everaldo.workshopmongo.dto.AuthorDTO;
 import com.everaldo.workshopmongo.repository.PostRepository;
 import com.everaldo.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,11 +37,13 @@ public class instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem","Vou viajar para São Paulo. Abraços!",maria );
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei feliz hoje!",maria );
-
-
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem","Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei feliz hoje!", new AuthorDTO(maria) );
+
+
+
         postRepository.saveAll(Arrays.asList(post1,post2));
 
 
